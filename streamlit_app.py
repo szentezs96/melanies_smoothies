ee15eba9d50b37cc1261904844e35b6e8b197124
szentezs96 <szentezs96@gmail.com>
2026-01-09 11:12:33 +0100

    Update streamlit_app.py
    updating file

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,12 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import pandas
 
+#Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
-st.write("""Choose the fruits you want in your custom Smoothie!""")
+st.write(
+    """Choose the fruits you want in your custom Smoothie!
+    """)
 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your SMoothie will be:', name_on_order)
@@ -18,19 +20,21 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-#st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
-    , max_selections = 5)
+    , max_selections = 5
+    )
 
 if ingredients_list:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
-        ingredients_string+= fruit_chosen + ' '
+        ingredients_string += fruit_chosen + ' '
+        
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
@@ -38,7 +42,8 @@ if ingredients_list:
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values (' """ + ingredients_string + """ ',' """+name_on_order+""" ')"""
+    my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order) 
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     time_to_insert = st.button('Submit Order')
 

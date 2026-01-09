8bd00fe8683dfb8d323d35f2830517c21df5bf30
szentezs96 <szentezs96@gmail.com>
2026-01-09 11:14:02 +0100

    Update streamlit_app.py
    fixing file

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if ingredients_list:
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order) 
-            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
     time_to_insert = st.button('Submit Order')
 
